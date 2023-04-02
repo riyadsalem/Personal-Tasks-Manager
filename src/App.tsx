24b1db5edbad7f96b1e5db55353fc916a77fca3d
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-04-02 00:37:31 -0700

    Creating Our First Page Release #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
+import { customTheme } from './theme/customTheme';
+
 const App: FC = (): ReactElement => {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Hello World!</h1>
+    </ThemeProvider>
+  );
 };
 
 export default App;
