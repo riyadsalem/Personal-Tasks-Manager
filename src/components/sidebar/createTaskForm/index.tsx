70b406a1ba4e5137a59c902e55dc38703f5f9e00
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-04-04 07:21:29 -0700

    Add enum data for SelectFiles Release #1

--- src/components/sidebar/createTaskForm/index.tsx
+++ src/components/sidebar/createTaskForm/index.tsx
@@ -4,6 +4,8 @@ import { TaskTitleField } from './_taskTitleField';
 import { TaskDescriptionField } from './_taskDescriptionField';
 import { TaskDateField } from './_taskDateField';
 import { TaskSelectField } from './_taskSelectField';
+import { Status } from './enums/Status';
+import { Priority } from './enums/Priority';
 
 export const CreateTaskForm: FC = (): ReactElement => {
   return (
@@ -27,8 +29,38 @@ export const CreateTaskForm: FC = (): ReactElement => {
           direction="row"
           spacing={2}
         >
-          <TaskSelectField />
-          <TaskSelectField />
+          <TaskSelectField
+            label="Status"
+            name="status"
+            items={[
+              {
+                value: Status.todo,
+                label: Status.todo.toUpperCase(),
+              },
+              {
+                value: Status.inProgress,
+                label: Status.inProgress.toUpperCase(),
+              },
+            ]}
+          />
+          <TaskSelectField
+            label="Priority"
+            name="priority"
+            items={[
+              {
+                value: Priority.low,
+                label: Priority.low,
+              },
+              {
+                value: Priority.normal,
+                label: Priority.normal,
+              },
+              {
+                value: Priority.high,
+                label: Priority.high,
+              },
+            ]}
+          />
         </Stack>
       </Stack>
     </Box>
