1e2e81e4225aca88e5c1130a3fd3f186fae67cf5
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-04-08 01:13:03 -0700

    Create Interface For Task Component Release #1

--- src/components/taskArea/task/index.tsx
+++ src/components/taskArea/task/index.tsx
@@ -2,5 +2,23 @@ import React, { FC, ReactElement } from 'react';
 import { Box } from '@mui/material';
 
 export const Task: FC = (): ReactElement => {
-  return <Box>Tasks Will Come Over Here</Box>;
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent="flex-start"
+      flexDirection="column"
+      mb={4}
+      p={2}
+      sx={{
+        width: '100%',
+        backgroundColor: 'background.paper',
+        borderRadius: '8px',
+        border: '1px solid',
+        borderColor: 'red',
+      }}
+    >
+      Tasks Will Come Over Here
+    </Box>
+  );
 };
