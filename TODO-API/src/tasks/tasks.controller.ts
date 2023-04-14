1bcd50d0eac076bb58289d26a8b04a49c1e1dc63
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-04-14 01:33:32 -0700

    Update Tasks (Created update method) Release #4

--- TODO-API/src/tasks/tasks.controller.ts
+++ TODO-API/src/tasks/tasks.controller.ts
@@ -1,8 +1,12 @@
 import { validationResult } from 'express-validator';
 import Task from './tasks.entity';
 import { AppDataSource } from '../../index';
-import { instanceToPlain } from 'class-transformer';
+import {
+  instanceToPlain,
+  plainToInstance,
+} from 'class-transformer';
 import { Request, Response } from 'express';
+import { UpdateResult } from 'typeorm';
 
 export class TasksController {
   // Method for the get route
@@ -91,8 +95,66 @@ export class TasksController {
     }
   }
 
-  public async update(req: Request, res: Response) {
-    console.log('This is update Method');
+  // Method for updating tasks
+  public async update(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
+
+    // Try to find if the tasks exists
+    let task: Task | null;
+
+    try {
+      task = await AppDataSource.getRepository(
+        Task,
+      ).findOne({
+        where: { id: req.body.id },
+      });
+    } catch (errors) {
+      return res
+        .json({ error: 'Internal Server Error' })
+        .status(500);
+    }
+
+    // Return 400 if task is null
+    if (!task) {
+      return res.status(404).json({
+        error: 'The task with given ID does not exist',
+      });
+    }
+
+    // Declare a variable for updatedTask
+    let updatedTask: UpdateResult;
+
+    // Update the task
+    try {
+      updatedTask = await AppDataSource.getRepository(
+        Task,
+      ).update(
+        req.body.id,
+        plainToInstance(Task, {
+          status: req.body.status,
+        }),
+      );
+
+      // Convert the updatedTask instance to an object
+      updatedTask = instanceToPlain(
+        updatedTask,
+      ) as UpdateResult;
+
+      return res.json(updatedTask).status(200);
+    } catch (errors) {
+      return res
+        .json({ error: 'Internal Server Error' })
+        .status(500);
+    }
   }
 }
 
