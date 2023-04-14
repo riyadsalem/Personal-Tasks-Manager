cf1a23177f0fd401de42a0b45f4dc93c615fcdf9
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-04-14 00:53:44 -0700

    Created (POST Tasks) Release #4

--- TODO-API/src/tasks/tasks.controller.ts
+++ TODO-API/src/tasks/tasks.controller.ts
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator';
 import Task from './tasks.entity';
 import { AppDataSource } from '../../index';
 import { instanceToPlain } from 'class-transformer';
@@ -5,6 +6,12 @@ import { Request, Response } from 'express';
 
 export class TasksController {
   // Method for the get route
+  /*constructor(
+    private taskRepository = AppDataSource.getRepository(
+      Task,
+    ),
+  ) {}*/
+
   public async getAll(
     req: Request,
     res: Response,
@@ -42,8 +49,45 @@ export class TasksController {
   }
 
   // Method for the post route
-  public async create(req: Request, res: Response) {
-    console.log('This is Create Method');
+  public async create(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
+
+    //Create a new instance of the Task
+    const newTask = new Task();
+
+    // Add the required properties to the Task object
+    newTask.title = req.body.title;
+    newTask.date = req.body.date;
+    newTask.description = req.body.description;
+    newTask.priority = req.body.priority;
+    newTask.status = req.body.status;
+
+    // Add the new task to the database
+    let createdTask: Task;
+
+    try {
+      createdTask = await AppDataSource.getRepository(
+        Task,
+      ).save(newTask);
+
+      // Convert the task instance to an object
+      createdTask = instanceToPlain(createdTask) as Task;
+
+      return res.json(createdTask).status(201);
+    } catch (errors) {
+      return res
+        .json({ error: 'Internal Server Error' })
+        .status(500);
+    }
   }
 }
 
