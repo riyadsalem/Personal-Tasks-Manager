0a6dcf3ba5188076f38ffdf25a568e08f5582780
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-04-14 05:41:15 -0700

    Integrating Tanstack Query Release #6

--- TODO/src/App.tsx
+++ TODO/src/App.tsx
@@ -2,13 +2,24 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 import { customTheme } from './theme/customTheme';
 import { Dashboard } from './pages/dashboard';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const App: FC = (): ReactElement => {
+  // Create a client
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
